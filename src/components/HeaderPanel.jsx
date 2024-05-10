025ad65803f30f3b71097684a5604edaa2c4b9bc
J0hnux <jc24.doliente@gmail.com>
2024-05-11 00:51:11 +0800

    Created necessary pages

---
+++ src/components/HeaderPanel.jsx
@@ -0,0 +1,15 @@
+const HeaderPanel = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderPanel;
---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,15 @@
+const MobileNav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MobileNav;
