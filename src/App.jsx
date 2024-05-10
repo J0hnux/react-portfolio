112a362153ba5c334dff926b0c8a0cc28e26808d
J0hnux <jc24.doliente@gmail.com>
2024-05-11 00:56:09 +0800

    Structured the project in a pleasing way

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import './App.scss'
+import "./App.scss";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import HeaderPanel from "./components/HeaderPanel";
+import Hero from "./components/Hero";
+import MobileNav from "./components/MobileNav";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
-      <div className="top-panel">
-        <div className="top-panel-content">
-          <p>John Clark Doliente</p>
-          <p>Front-End Web Developer</p>
-        </div>
-      </div>
-      <div className="home-panel">
-        HOME
-      </div>
+      <HeaderPanel />
+      <MobileNav />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
