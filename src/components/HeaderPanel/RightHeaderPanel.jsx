783f533b44080a58f84137fad311807c9cd9fbd8
J0hnux <jc24.doliente@gmail.com>
2024-05-11 17:57:54 +0800

    Simple structuring

---
+++ src/components/HeaderPanel/RightHeaderPanel.jsx
@@ -0,0 +1,16 @@
+const RightHeaderPanel = () => {
+  return (
+    <div className="right__header_panel">
+      <ul>
+        <li>
+          <button>O</button>
+        </li>
+        <li>
+          <button className="btn__resume">Resume</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RightHeaderPanel;
