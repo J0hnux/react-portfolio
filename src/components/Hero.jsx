2f964e141551af7e317496cd813c1a55469d68c8
J0hnux <jc24.doliente@gmail.com>
2024-05-12 01:18:05 +0800

    Refactored current structure and added inner__container class

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="hero container section">
+      <div className="hero__inner__container">
+        <div className="left_hero">
+          <h3>My name is Clark.</h3>
+          <h1>FRONT-END WEB DEVELOPER.</h1>
+          <p>
+            I am a self-motivated, passionate self-taught Front-end web
+            developer.
+          </p>
+        </div>
+        <img src="/my-pic2.jpeg" alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
