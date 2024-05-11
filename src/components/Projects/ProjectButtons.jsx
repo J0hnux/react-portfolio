7c407d0bfd77a253f54752c9d6b3c22b5d6c3562
J0hnux <jc24.doliente@gmail.com>
2024-05-11 16:51:25 +0800

    Created a separate component for Project Buttons

---
+++ src/components/Projects/ProjectButtons.jsx
@@ -0,0 +1,10 @@
+const ProjectButtons = () => {
+  return (
+    <div>
+      <button>Visit website</button>
+      <button>View on Github</button>
+    </div>
+  );
+};
+
+export default ProjectButtons;
