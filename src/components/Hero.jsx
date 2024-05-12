e3c46db409f9caa52b413e710d191f455b4b7733
J0hnux <jc24.doliente@gmail.com>
2024-05-12 20:50:15 +0800

    Revised the hero structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,21 @@
+import "./Hero.scss";
+
 const Hero = () => {
   return (
     <section className="hero container section">
       <div className="hero__inner__container">
         <div className="left_hero">
           <h3>My name is Clark.</h3>
-          <h1>FRONT-END WEB DEVELOPER.</h1>
+          <h1>
+            FRONT-END
+            <br />
+            WEB DEVELOPER.
+          </h1>
           <p>
-            I am a self-motivated, passionate self-taught Front-end web
-            developer.
+            I am a self-taught Front-end web developer with a passion in web
+            development and UI/UX.
           </p>
         </div>
-        <img src="/my-pic2.jpeg" alt="" />
       </div>
     </section>
   );
