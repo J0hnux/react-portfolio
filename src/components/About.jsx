1bdab882f753aa3d269202cc04ebba32fd9d98d1
J0hnux <jc24.doliente@gmail.com>
2024-05-13 22:53:44 +0800

    Added svg for the technologies section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
+import "./About.scss";
+
 const About = () => {
   return (
-    <section className="about container section">
+    <section className="about container section" id="about">
       <div className="about__left">
         <h1>About</h1>
         <div className="about__box">
           <p>
-            Hello! I'm Clark, a self-taught <span>Frontend</span> web developer
+            Hello! I'm Clark, a self-taught{" "}
+            <span className="frontend__color">Frontend</span> web developer
             based on Philippines. Ever since I was a kid it has been my dream to
             create things. Be it web based apps, games or software apps.
           </p>
@@ -15,21 +18,36 @@ const About = () => {
             ASAP so that I can use it already on my projects.
           </p>
           <p>
-            My previous project is a fullstack blog site. Though I am not
+            My most recent project was a fullstack blog site using NextJS and
+            various other technologies for the backend. Though I am not
             confident with my backend to be considered as a fullstack developer,
-            but we will get there.
+            but we will soon get there.
           </p>
         </div>
         <div className="tech__stack">
           <h3>Technologies</h3>
           <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript (ES6+)</li>
-            <li>ReactJS</li>
-            <li>NextJS</li>
-            <li>TailwindCSS</li>
-            <li>SASS</li>
+            <li>
+              <img src="/Technologies/html.svg" alt="html" />
+            </li>
+            <li>
+              <img src="/Technologies/css3.svg" alt="css3" />
+            </li>
+            <li>
+              <img src="/Technologies/javascript.svg" alt="javascript" />
+            </li>
+            <li>
+              <img src="/Technologies/react.svg" alt="reactjs" />
+            </li>
+            <li>
+              <img src="/Technologies/nextjs.svg" alt="nextjs" />
+            </li>
+            <li>
+              <img src="/Technologies/tailwind.svg" alt="tailwind" />
+            </li>
+            <li>
+              <img src="/Technologies/sass.svg" alt="sass" />
+            </li>
           </ul>
         </div>
       </div>
