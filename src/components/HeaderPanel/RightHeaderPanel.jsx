9b5bc28578fc34cbeed69db82e60a3078cb2d6c9
J0hnux <jc24.doliente@gmail.com>
2024-05-14 00:30:54 +0800

    Separated this from another nav for easier styling later

--- src/components/HeaderPanel/RightHeaderPanel.jsx
+++ src/components/HeaderPanel/RightHeaderPanel.jsx
@@ -1,15 +1,9 @@
 const RightHeaderPanel = () => {
   return (
-    <div className="right__header_panel">
-      <ul>
-        <li>
-          <button>O</button>
-        </li>
-        <li>
-          <button className="btn__resume">Resume</button>
-        </li>
-      </ul>
-    </div>
+    <nav className="right__header_panel">
+      <button className="btn__toggle_darklight">O</button>
+      <button className="btn__resume">Resume</button>
+    </nav>
   );
 };
 
