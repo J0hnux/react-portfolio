e082a133d18449efa6667b2cbf3a2e13657b433f
J0hnux <jc24.doliente@gmail.com>
2024-05-14 00:27:46 +0800

    Basic structure

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,5 +1,21 @@
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+import ProjectButtons from "./ProjectButtons";
+
+const ProjectCard = (props) => {
+  return (
+    <div className="project__card_container">
+      <img src="" alt="project_image" />
+      <div className="project_card_content">
+        <h3>{props.name}</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          laborum illum cumque iste inventore quaerat! Cum exercitationem
+          obcaecati at delectus quibusdam, error deserunt labore mollitia nulla
+          repudiandae nam fugit nesciunt?
+        </p>
+        <ProjectButtons />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
