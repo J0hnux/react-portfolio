7655f55d92a29850acb150a5fe0e164bf723a30d
J0hnux <jc24.doliente@gmail.com>
2024-05-14 00:29:36 +0800

    added anchor tag for each nav links

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,11 +1,17 @@
 const MobileNav = () => {
   return (
-    <div>
+    <div className="mobile-nav">
       <nav>
         <ul>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </nav>
     </div>
