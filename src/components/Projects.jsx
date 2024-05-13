7abbb9e95b3839ba87d9f59c34dbc8a756da5311
J0hnux <jc24.doliente@gmail.com>
2024-05-14 00:30:05 +0800

    Added projectcard for testing, will change it later to make it work dynamically

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,17 @@
+import ProjectCard from "./Projects/ProjectCard";
+import "./Projects.scss";
+
 const Projects = () => {
-  return <section>Projects</section>;
+  return (
+    <section className="projects container section" id="projects">
+      <h1>Projects</h1>
+      <div className="project__cards">
+        <ProjectCard name="Real Estate" />
+        <ProjectCard name="Car Rental" />
+        <ProjectCard name="Blog Site" />
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
