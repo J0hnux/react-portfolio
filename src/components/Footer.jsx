6269cee1a081289918484439425967abbe9c7ccb
J0hnux <jc24.doliente@gmail.com>
2024-05-14 00:28:46 +0800

    Populated footer section with information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="container section">
+      Copyright © 2024. All rights are reserved
+      <div className="socials__container">
+        <a href="">Linkedin</a>
+        <a href="">Github</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
