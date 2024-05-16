0f059305d8ffb4b14f4d9fa6d51f55e89a6c10bc
J0hnux <jc24.doliente@gmail.com>
2024-05-17 02:04:01 +0800

    Added icons for the buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import "./Footer.scss";
+
 const Footer = () => {
   return (
-    <footer className="container section">
+    <footer className="footer container section">
       Copyright © 2024. All rights are reserved
       <div className="socials__container">
-        <a href="">Linkedin</a>
-        <a href="">Github</a>
+        <a href="https://www.linkedin.com/in/jclark007/" target="_blank">
+          <img src="/ButtonIcons/linkedin.svg" alt="linkedin" />
+        </a>
+        <a href="https://github.com/J0hnux" target="_blank">
+          <img src="/ButtonIcons/github.svg" alt="visit source on github" />
+        </a>
       </div>
     </footer>
   );
