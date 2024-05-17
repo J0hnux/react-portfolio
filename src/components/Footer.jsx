287248e53d835c624c206aa5cea912056f870b79
J0hnux <jc24.doliente@gmail.com>
2024-05-17 18:54:20 +0800

    Encapsulated copyright inside p tag for proper semantics and for styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer container section">
-      Copyright © 2024. All rights are reserved
+      <p>Copyright © 2024. All rights are reserved</p>
       <div className="socials__container">
         <a href="https://www.linkedin.com/in/jclark007/" target="_blank">
           <img src="/ButtonIcons/linkedin.svg" alt="linkedin" />
