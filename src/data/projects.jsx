ac1099faff4f3d6c3664fbb84429b92d01d8e45b
J0hnux <jc24.doliente@gmail.com>
2024-06-18 17:54:21 +0800

    Data for the project card for dynamic generation of components

---
+++ src/data/projects.jsx
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    imgURL: "/Portfolios/my-car-rental.png",
+    imgAlt: "Car Rental",
+    techStacks: [
+      { imgURL: "/Technologies/react.svg", imgAlt: "reactjs" },
+      { imgURL: "/Technologies/nextjs.svg", imgAlt: "nextjs" },
+      { imgURL: "/Technologies/tailwind.svg", imgAlt: "tailwind" },
+      { imgURL: "/Technologies/sass.svg", imgAlt: "sass" },
+    ],
+    name: "Clark Cars",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque officia, corporis hic saepe eveniet eum ad necessitatibus reprehenderit, optio consequatur excepturi odit dolor libero provident adipisci ullam! Nemo, delectus eligendi.",
+    websiteLink: "https://www.google.com",
+    githubLink: "https://www.github.com",
+  },
+];
+
+export default projects;
