5352322e20ffbdf763401cf8b4fc909bff938ace
J0hnux <jc24.doliente@gmail.com>
2024-06-28 11:05:26 +0800

    removed button

--- src/components/HeaderPanel/RightHeaderPanel.jsx
+++ src/components/HeaderPanel/RightHeaderPanel.jsx
@@ -1,7 +1,6 @@
 const RightHeaderPanel = () => {
   return (
     <nav className="right__header_panel">
-      <button className="btn__toggle_darklight">O</button>
       <button className="btn__resume">Resume</button>
     </nav>
   );
