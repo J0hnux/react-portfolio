b0784f903f1b6e3f5f5142d1a988f89ef0d11757
J0hnux <jc24.doliente@gmail.com>
2024-07-12 19:06:05 +0800

    Added link to my resume

--- src/components/HeaderPanel/RightHeaderPanel.jsx
+++ src/components/HeaderPanel/RightHeaderPanel.jsx
@@ -1,7 +1,11 @@
 const RightHeaderPanel = () => {
   return (
     <nav className="right__header_panel">
-      <button className="btn__resume">Resume</button>
+      <button className="btn__resume">
+        <a href="https://drive.google.com/file/d/1-q1oczlqwSk-xbfQmXakiI8UsOk-0_sp/view?usp=sharing">
+          Resume
+        </a>
+      </button>
     </nav>
   );
 };
