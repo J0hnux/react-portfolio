b3ca4a149303745217807f852a2ce9625a2998d2
J0hnux <jc24.doliente@gmail.com>
2024-07-12 19:03:36 +0800

    Removed MobileNav from the App Components Tree

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import HeaderPanel from "./components/HeaderPanel";
 import Hero from "./components/Hero";
-import MobileNav from "./components/MobileNav";
 import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <HeaderPanel />
-      <MobileNav />
       <main>
         <Hero />
         <About />
