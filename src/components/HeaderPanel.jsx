3d7598513fcbfdd2ff4c97dbac01c3d38e7d26db
J0hnux <jc24.doliente@gmail.com>
2024-07-12 19:03:51 +0800

    Added Mobile Nav function

--- src/components/HeaderPanel.jsx
+++ src/components/HeaderPanel.jsx
@@ -1,9 +1,24 @@
 import RightHeaderPanel from "./HeaderPanel/RightHeaderPanel";
 import "./HeaderPanel.scss";
+import { useState } from "react";
+import MobileNav from "./MobileNav";
 
 const HeaderPanel = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+
+    if (isOpen) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  };
+
   return (
     <header className="header container">
+      <MobileNav isOpen={isOpen} toggleMenu={toggleMenu} />
       <nav className="left-nav">
         <ul>
           <li>
@@ -18,7 +33,7 @@ const HeaderPanel = () => {
         </ul>
       </nav>
       {/* 3 Bar Line Button */}
-      <button className="mobile__nav_btn">
+      <button className="mobile__nav_btn" onClick={toggleMenu}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
