bee5b693c08f65b1ad075ff4323c38f96588bcf4
J0hnux <jc24.doliente@gmail.com>
2024-07-12 19:29:12 +0800

    Changed button to a tag so users can see what link they're going to get redirected when they click

--- src/components/HeaderPanel/RightHeaderPanel.jsx
+++ src/components/HeaderPanel/RightHeaderPanel.jsx
@@ -1,11 +1,13 @@
 const RightHeaderPanel = () => {
   return (
     <nav className="right__header_panel">
-      <button className="btn__resume">
-        <a href="https://drive.google.com/file/d/1-q1oczlqwSk-xbfQmXakiI8UsOk-0_sp/view?usp=sharing">
-          Resume
-        </a>
-      </button>
+      <a
+        className="btn__resume"
+        href="https://drive.google.com/file/d/1-q1oczlqwSk-xbfQmXakiI8UsOk-0_sp/view?usp=sharing"
+        target="_blank"
+      >
+        Resume
+      </a>
     </nav>
   );
 };
