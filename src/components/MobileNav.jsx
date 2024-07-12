20df734c3425d39857dc9bf7d55dd9001e901c57
J0hnux <jc24.doliente@gmail.com>
2024-07-12 19:05:20 +0800

    removed the x button and put a props function to toggleMenu upon click of links

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,23 +1,26 @@
 import "./MobileNav.scss";
 
-const MobileNav = () => {
+const MobileNav = (props) => {
   return (
-    <div className="mobile-nav">
-      <button>X</button>
-      <nav>
-        <ul>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={`mobile-nav ${props.isOpen ? "is-open" : ""} `}>
+      <ul>
+        <li>
+          <a href="#about" onClick={props.toggleMenu}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects" onClick={props.toggleMenu}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact" onClick={props.toggleMenu}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
