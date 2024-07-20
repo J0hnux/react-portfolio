6e8ac48902fd45c0acdf760bbb64da4767acdc1c
J0hnux <jc24.doliente@gmail.com>
2024-07-20 12:40:35 +0800

    Updated project description

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -10,7 +10,7 @@ const projects = [
     ],
     name: "Clark Cars",
     description:
-      "Clark Cars is a car rental website developed using NextJS and ReactJS. To style the website a combination of Tailwind and SCSS were used. Clark Cars is almost a complete website, with about and contact page that utilises the power of captcha to distinguish bots from humans. However, the captcha is very simple which asks a random mathematical question that even a kindergarten student can answer.",
+      "Clark Cars is a car rental website developed using NextJS and ReactJS. To style the website a combination of Tailwind and SCSS were used. Clark Cars is like almost any car rental websites out there, with a contact page that utilises the power of captcha to distinguish bots from humans. What I like most about the contact page is the captcha where I created all the mathematical questions with Paint with questions that even a kindergarten student can answer. The only thing that Clark Cars doesn't have yet is a fleet of cars that can actually be rented.",
     websiteLink: "https://www.google.com",
     githubLink: "https://www.github.com",
   },
