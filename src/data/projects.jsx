297fc9df0e24c8d744fea5cb2f1bc1a08c61b06d
J0hnux <jc24.doliente@gmail.com>
2026-03-08 03:29:37 +0800

    Updated

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -11,8 +11,8 @@ const projects = [
     name: "Clark Cars",
     description:
       "Clark Cars is a car rental website developed using NextJS and ReactJS. To style the website a combination of Tailwind and SCSS were used. The website is not yet fully developed, what it lacks is the backend and a few things as well. Also, there are other design aspects that it lacks, like animation, loading skeleton and so much more. I am thinking of changing the Category section into a centered stacked carousel which only appears on low width screens to make it more user friendly and for aesthetic as well.",
-    websiteLink: "https://clark-cars-rental.vercel.app/",
-    githubLink: "https://github.com/J0hnux/my-car-rental",
+    websiteLink: "https://elite-cars-rental.vercel.app/",
+    githubLink: "https://github.com/J0hnux/nextjs-car-rental",
   },
 ];
 
